fa4188b44a966f819767aace847afd8c54e54c0c
r00tgate <mike.wylie@corporateblue.com>
2017-09-11 06:20:32 -0700

    Fixed output file
    Single > was causing blank file.
    Forgot to add $ for vardomain.

--- dns-host-brute.sh
+++ dns-host-brute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ask the user for their domain name
-echo What domain name would you like to enumerate?
+echo What domain name would you like to enumerate (e.g. example.com)?
 read vardomain
 for i in $(cat ./subdomain-list.txt); do
-	host $i.vardomain > dns-output.txt
+	host $i.$vardomain >> dns-output.txt
 done
